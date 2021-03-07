1a6cfb011fbc1d59940d972db8e68c6c20e95f04
Your Name <you@example.com>
2021-03-07 13:26:39 -0700

    custom event subscribers

--- src/EventSubscriber/VideoCreatedSubscriber.php
+++ src/EventSubscriber/VideoCreatedSubscriber.php
@@ -4,6 +4,10 @@ namespace App\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpFoundation\Response;
+
 class VideoCreatedSubscriber implements EventSubscriberInterface
 {
     public function onVideoCreatedEvent($event)
@@ -11,10 +15,36 @@ class VideoCreatedSubscriber implements EventSubscriberInterface
         dump("subscriber: ". $event->video->title);
     }
 
+    // public function onKernelResponse(ResponseEvent $event)
+    // {
+    //     $response = new Response('dupa');
+    //     // $event->setResponse($response);
+    // }
+
+    public function onKernelResponse1(ResponseEvent $event)
+    {
+        $response = new Response('dupa1');
+        // $event->setResponse($response);
+        dump('1');
+    }
+
+    public function onKernelResponse2(ResponseEvent $event)
+    {
+        $response = new Response('dupa2');
+        // $event->setResponse($response);
+        dump('2');
+    }
+
     public static function getSubscribedEvents()
     {
         return [
            'video.created.event' => 'onVideoCreatedEvent',
+        //    KernelEvents::RESPONSE => 'onKernelResponse',
+            KernelEvents::RESPONSE => [
+                ['onKernelResponse1', 2],
+                ['onKernelResponse2', 1],
+            ],
+
         ];
     }
 }
